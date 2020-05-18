f6157bbe2238ac52134f34e1dcbc6b2d5ad2f049
suvin-241 <suvin241@gmail.com>
2020-05-18 15:52:04 +0100

    cross orgin

--- src/main/java/com/pharma/itsmypharma/controller/UserController.java
+++ src/main/java/com/pharma/itsmypharma/controller/UserController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import com.pharma.itsmypharma.util.PharmaConstants;
 
 
 @RestController
+@CrossOrigin
 public class UserController {
 
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
