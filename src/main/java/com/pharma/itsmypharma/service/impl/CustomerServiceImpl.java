82e12c255cbfadefe5f36304eff72a132297f1fa
suvin-241 <suvin241@gmail.com>
2020-08-10 19:35:18 +0100

    added

--- src/main/java/com/pharma/itsmypharma/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/pharma/itsmypharma/service/impl/CustomerServiceImpl.java
@@ -1,8 +1,12 @@
 package com.pharma.itsmypharma.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.pharma.itsmypharma.model.CustomerVo;
 import com.pharma.itsmypharma.service.CustomerService;
 
+
+@Service
 public class CustomerServiceImpl implements CustomerService{
 
 	@Override
